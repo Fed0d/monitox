47738c0d8a2df4721e55ff7420cb393fb385f47e
m.s.kozhanoff <m.s.kozhanoff@ispras.ru>
2024-09-08 17:09:16 +0300

    prometheus and grafana started

--- run_bot.py
+++ run_bot.py
@@ -1,15 +1,18 @@
 import asyncio
+from prometheus_client import start_http_server, Summary
 
 from monitox.bot import bot, dp, logger
 from monitox.commands import set_commands
 from monitox.handlers import router
 
+REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
 
 async def main():
+    start_http_server(8001)
+
     dp.include_router(router)
     dp.startup.register(set_commands)
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     try:
