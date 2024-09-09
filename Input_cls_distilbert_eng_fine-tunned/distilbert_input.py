431ab4649b6b700c55d8ac98be726888002f62fc
bogdan01m <152898065+bogdan01m@users.noreply.github.com>
2024-09-09 23:27:50 +0500

    Update distilbert_input.py

--- Input_cls_distilbert_eng_fine-tunned/distilbert_input.py
+++ Input_cls_distilbert_eng_fine-tunned/distilbert_input.py
@@ -9,7 +9,7 @@ pl.seed_everything(RANDOM_SEED)
 BERT_MODEL_NAME = 'distilbert/distilbert-base-uncased-finetuned-sst-2-english'
 tokenizer = BertTokenizer.from_pretrained(BERT_MODEL_NAME)
 
-class Train_input_classifier(pl.LightningModule):
+class Input_classifier(pl.LightningModule):
 
   def __init__(self, n_classes: int, n_training_steps=None, n_warmup_steps=None):
     super().__init__()
@@ -27,66 +27,6 @@ class Train_input_classifier(pl.LightningModule):
     if labels is not None:
         loss = self.criterion(output, labels)
     return loss, output
-
-  def training_step(self, batch, batch_idx):
-    input_ids = batch["input_ids"]
-    attention_mask = batch["attention_mask"]
-    labels = batch["labels"]
-    loss, outputs = self(input_ids, attention_mask, labels)
-    self.log("train_loss", loss, prog_bar=True, logger=True)
-    return {"loss": loss, "predictions": outputs, "labels": labels}
-
-  def validation_step(self, batch, batch_idx):
-    input_ids = batch["input_ids"]
-    attention_mask = batch["attention_mask"]
-    labels = batch["labels"]
-    loss, outputs = self(input_ids, attention_mask, labels)
-    self.log("val_loss", loss, prog_bar=True, logger=True)
-    return loss
-
-  def test_step(self, batch, batch_idx):
-    input_ids = batch["input_ids"]
-    attention_mask = batch["attention_mask"]
-    labels = batch["labels"]
-    loss, outputs = self(input_ids, attention_mask, labels)
-    self.log("test_loss", loss, prog_bar=True, logger=True)
-    return loss
-
-  def on_training_epoch_end(self, outputs):
-
-    labels = []
-    predictions = []
-    for output in outputs:
-      for out_labels in output["labels"].detach().cpu():
-        labels.append(out_labels)
-      for out_predictions in output["predictions"].detach().cpu():
-        predictions.append(out_predictions)
-
-    labels = torch.stack(labels).int()
-    predictions = torch.stack(predictions)
-
-    for i, name in enumerate(LABEL_COLUMNS):
-      class_f1 = f1_score(predictions[:, i], labels[:, i],average='weighted' )
-      self.logger.experiment.add_scalar(f"{name}_f1/Train", class_f1, self.current_epoch)
-
-
-  def configure_optimizers(self):
-
-    optimizer = AdamW(self.parameters(), lr=2e-5)
-
-    scheduler = get_linear_schedule_with_warmup(
-      optimizer,
-      num_warmup_steps=self.n_warmup_steps,
-      num_training_steps=self.n_training_steps
-    )
-
-    return dict(
-      optimizer=optimizer,
-      lr_scheduler=dict(
-        scheduler=scheduler,
-        interval='step'
-      )
-    )
 N_EPOCHS = 2
 BATCH_SIZE = 32
 LABEL_COLUMNS=['benign', 'jailbreak', 'prompt_injection']
@@ -94,7 +34,7 @@ warmup_steps=185
 total_training_steps=926
 checkpoint = torch.load("trained_model.ckpt")
 # Создание нового экземпляра модели
-loaded_model = Train_input_classifier(n_classes=len(LABEL_COLUMNS), n_training_steps=total_training_steps, n_warmup_steps=warmup_steps)
+loaded_model = Input_classifier(n_classes=len(LABEL_COLUMNS), n_training_steps=total_training_steps, n_warmup_steps=warmup_steps)
 
 # Загрузка весов и гиперпараметров из чекпоинта
 loaded_model.load_state_dict(checkpoint["state_dict"])
