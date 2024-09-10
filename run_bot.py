772a946b9155423ce64b62d7b604c1f52fc77c7f
Fed0d <loldffgf@yandex.ru>
2024-09-10 23:10:07 +0300

    Edit db

--- run_bot.py
+++ run_bot.py
@@ -1,11 +1,13 @@
 import asyncio
-from prometheus_client import start_http_server, Summary
+
+from prometheus_client import Summary, start_http_server
 
 from monitox.bot import bot, dp, logger
 from monitox.commands import set_commands
 from monitox.handlers import router
 
-REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+REQUEST_TIME = Summary("request_processing_seconds", "Time spent processing request")
+
 
 async def main():
     start_http_server(8001)
@@ -13,6 +15,7 @@ async def main():
     dp.include_router(router)
     dp.startup.register(set_commands)
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     try:
